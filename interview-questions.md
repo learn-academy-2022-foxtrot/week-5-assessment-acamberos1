cca302b89234ded39c181110f57e7c0690feda7a
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-10-12 12:57:04 -0700

    updated

--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a gem?
 
-Your answer:
+Your answer: a gem is a repository to save the data you worked on
 
-Researched answer:
+Researched answer: A gem is library or chunk of code for a certain functionality 
 
 2. What does the acronym CRUD stand for?
 
-Your answer:
+Your answer: create, read, utilize, delete
 
-Researched answer:
+Researched answer: it stands for create, update , and delete
 
 3. What is Ruby on Rails?
 
-Your answer:
+Your answer: ruby on rails is a framework adding more functionality for ruby for web development and database development
 
-Researched answer:
+Researched answer: Ruby on Rails is a backend web app framework made in Ruby
 
 4. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: a relational database is a database where the components communicate with each other , there are different types of databases
 
-Researched answer:
+Researched answer: Relational databases are a kinds of database that record and organize data  with defined relationships for easier acccess to them
 
 5. What are primary keys? Why are they important?
 
-Your answer:
+Your answer:primary keys are data pairs that help differentiate and invoke values from the data whether it be arrays, objects or hashes
 
-Researched answer:
+Researched answer: primary keys are unique fields in a table to identify each row
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: RESTful routing is a set of protocols used in multiple different languages to create working, reusable routes
 
-2. JSON:
+2. JSON: JavaScript Object Notation (JSON) is a standard text-based format for representing structured data based on JavaScript object syntax. It
 
-3. ERB:
+3. ERB: returns a string in the same character encoding as the input string.
 
-4. Params:
+4. Params:  parameter is a  variable in computer programming l that is used to pass information between functions or methods
 
-5. API:
+5. API: software that lets applications communicate with each other
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -2,24 +2,103 @@
 # MINASWAN
 
 # --------------------1) Create a method that takes in an array of words and a single letter and returns an array of all the words containing that particular letter. Use the test variables provided.
+#method will be named as a filter of some sort 
+# if index value has letter o or t remove the index value all together and just print the rest
+# parameter will be array to pass in the beverage array
+#this attempts only outputs nil
 
-beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+ beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+# def word_filter (array)
+#     if array.index.inlclude('o')
+#     index.clear
+    
+#     end 
+# end
+#  p word_filter(beverages_array)
 
-letter_o = 'o'
-# Expected output: ['coffee', 'soda water']
-letter_t = 't'
-# Expected output: ['tea', 'water', 'soda water']
 
+# { |string| string.upcase }
+# filter would iterate each element and if string had o it would push into empty array
+# beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+
+# def word_filter do |arr|
+#      emptyArr = []
+#     arr.filter {|string| 
+#             string.include?('o')
+#             emptyArr.push(string)}
+    
+# end
+
+# method will intake arr and value to see value in arr
+# arr select will filter out words based on letters it has
+#if it includes a certain value or latter it will print them out 
+# beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+# def word_filter(arr,value)
+#     arr.select {|word| word.include?(value)}
+# end
+
+
+# letter_o = 'o'
+# # Expected output: ['coffee', 'soda water']
+# letter_t = 't'
+# # Expected output: ['tea', 'water', 'soda water']
+
+# p word_filter(beverages_array,letter_t)
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
 
-us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Vermont'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Vermont', 'Washington'] 
-
+us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Vermont'] }
+#variable us_states undefined, I commented out the function to see if it would print without it and it did
+# i placed the array in multiple places incase it would work and read it but no luck
+# after trial and error using obj, and putting different extenstions in front of it this will address the objects values and also un nest them while also putting them in alphabetical order.
+def un_nest(obj)
+obj.values.flatten.sort()
+end
+p un_nest(us_states)
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
+# bike will have method determining the model, amount of wheels and speed,
 
+class bike
+attr_accessor :model, :wheels, :current_speed
+    
+def initialize (model)
+    @model = model
+    @wheels = 2
+    @current_speed = 0
+end
+
+def bike_info
+    p "The #{@model} has #{@wheels} wheels and is going #{@current_speed}"
+end
+
+def pedal_faster (speed)
+    @speed = speed
+    @current_speed = @current_speed + @speed
+end
+
+def brake (speed)
+    @speed = speed
+    @current_speed = @current_speed - @speed
+    if @current_speed < 0
+        @current_speed = 0
+    else @current_speed = @current_speed
+    end
+end
+end
+
+
+my_bike = Bike.new("Trike")
+p my_bike.pedal_faster(10) 
+# Expected output example: my_bike.pedal_faster(10) => 10
+p my_bike.pedal_faster(18)
+# Expected output example: my_bike.pedal_faster(18) => 28
+p my_bike.brake(5)
+# Expected output example: my_bike.brake(5) => 23
+p my_bike.brake(25)
+# Expected output example: my_bike.brake(25) => 0
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
 
